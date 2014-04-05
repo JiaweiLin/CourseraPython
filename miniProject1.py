6bd0bb3db4207ad89318d1d60866262e0aeff65e
Jiawei Lin <jiaweilin02@gmail.com>
2014-04-05 17:06:20 +0800

    Added mini project 1, which is completed.

---
+++ miniProject1.py
@@ -0,0 +1,89 @@
+import random
+# Rock-paper-scissors-lizard-Spock template
+
+# The key idea of this program is to equate the strings
+# "rock", "paper", "scissors", "lizard", "Spock" to numbers
+# as follows:
+#
+# 0 - rock
+# 1 - Spock
+# 2 - paper
+# 3 - lizard
+# 4 - scissors
+
+# helper functions
+
+def name_to_number(name):
+    # convert name to number using if/elif/else
+    # don't forget to return the result!
+    if(name=="rock"):
+        return 0
+    elif(name=="Spock"):
+        return 1
+    elif(name=="paper"):
+        return 2
+    elif(name=="lizard"):
+        return 3
+    else:
+        return 4
+
+def number_to_name(number):
+    # convert number to a name using if/elif/else
+    # don't forget to return the result!
+    if(number==0):
+        return "rock"
+    elif(number==1):
+        return "Spock"
+    elif(number==2):
+        return "paper"
+    elif(number==3):
+        return "lizard"
+    else:
+        return "scissors"
+
+def rpsls(player_choice):
+    # print a blank line to separate consecutive games
+    print ""
+
+    # handling of invalid input
+    if(player_choice not in ("rock","Spock","paper","lizard","scissors")):
+        print "%s is an invalid input." % player_choice
+        return ""
+
+    # print out the message for the player's choice
+    print "Player chooses " + player_choice
+
+    # convert the player's choice to player_number using the function name_to_number()
+    player_number = name_to_number(player_choice)
+
+    # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(0,5)
+
+    # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
+
+    # print out the message for computer's choice
+    print "Computer chooses " + comp_choice
+
+    # compute difference of comp_number and player_number modulo five
+    difference = comp_number-player_number
+
+    # use if/elif/else to determine winner, print winner message
+    a = difference % 5
+    if(a==1):
+        print "Computer wins!"
+    elif(a==2):
+        print "Computer wins!"
+    elif(a==3):
+        print "Player wins!"
+    elif(a==4):
+        print "Player wins!"
+    else:
+        print "Player and computer tie!"
+
+# test your code - LEAVE THESE CALLS IN YOUR SUBMITTED CODE
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
